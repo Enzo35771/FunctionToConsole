31f45978868caef0ec941b55e5c83008e4a97d7b
Enzo35771 <st10035771@vcconnect.edu.za>
2024-09-19 14:04:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace FunctionToConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var json = " {\"name\":\"John\"}";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("https://rwanwig.azurewebsites.net/api/Function1", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseString);
+                }
+                else
+                {
+                    Console.WriteLine("Error : " + response.StatusCode);
+                }
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FunctionToConsole
+ 09/19/2024 console app to run a function
